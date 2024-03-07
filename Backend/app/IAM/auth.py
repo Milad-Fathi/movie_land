2ad02c28a0d15321f92db03ec845ec322fb09634
milad fathi <miladfathimf7@gmail.com>
2024-03-07 11:00:03 -0800

    login and sign up API's added to the project

--- Backend/app/IAM/auth.py
+++ Backend/app/IAM/auth.py
@@ -44,7 +44,7 @@ class PersonRequest(BaseModel):
     # role: str = Field(min_length=1)
     email: str = Field(min_length=1)
     phone_number:int = Field(min_length=10, max_length=16)
-    # is_active: bool 
+    # is_active: bool = Field(default=False) 
     
 class Token(BaseModel):
     access_token: str
@@ -52,6 +52,121 @@ class Token(BaseModel):
 
 
 
-@router.get("/")
-async def get_hello():
-    return{"hello": "world"}
+
+
+
+
+# **************************different**********************************************************************
+# To check if person's username and password is correct
+def authenticate_user(user_name: str, password: str, db):
+    user = db.query(Person).filter(Person.user_name == user_name).first()
+    if not user:
+        # return {"msg": "User not signed up"}
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        # return {"msg": "Wrong password"}
+        return False
+    # if user.is_active == False:
+    #     # return {"msg": "Activate first"}
+    #     return False
+    return user
+
+
+# to create token
+def create_access_token(user_name: str, person_id: int, role: str, expires_delta: timedelta):
+    encode = {'sub': user_name, 'id': person_id, 'role': role}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+# to check if the token is correct / the person is authorized
+# to decode a token
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        person_id: int = payload.get('id')
+        user_role: str = payload.get('role')
+        if username is None or person_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user/admin.')
+        return {'username': username, 'id': person_id, 'user_role': user_role}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user/admin.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# creating user
+@router.post("/signup_user", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: PersonRequest):
+    user = db.query(Person).filter(Person.user_name == create_user_request.user_name).first()
+    if not user:
+        create_user_model = Person(
+            email=create_user_request.email,
+            user_name=create_user_request.user_name,
+            role="user",
+            hashed_password=bcrypt_context.hash(create_user_request.plain_text_password),
+            phone_number = create_user_request.phone_number,
+            is_active = False
+        )
+
+        db.add(create_user_model)
+        db.commit()
+    else:
+        return{"msg":"The username is already existing!!!"}
+
+
+
+# creating admin
+@router.post("/signup_admin", status_code=status.HTTP_201_CREATED)
+async def create_admin(db: db_dependency,
+                      create_admin_request: PersonRequest):
+    user = db.query(Person).filter(Person.user_name == create_admin_request.user_name).first()
+    if not user:
+        create_admin_model = Person(
+            email=create_admin_request.email,
+            user_name=create_admin_request.user_name,
+            role="admin",
+            hashed_password=bcrypt_context.hash(create_admin_request.plain_text_password),
+            phone_number = create_admin_request.phone_number,
+            is_active = False
+        )
+
+        db.add(create_admin_model)
+        db.commit()
+    else:
+        return{"msg":"The username is already existing!!!"}
+
+# *******************person req. class instead ....
+# login for user/admin
+@router.post("token",response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user/admin.')
+    
+    token = create_access_token(user.user_name, user.id, user.role, timedelta(minutes=120))
+
+    return {'access_token': token, 'token_type': 'bearer'}
+
+
+@router.get("read_user")
+async def read_all_users(username:str,db:db_dependency):
+    user = db.query(Person).filter(Person.user_name == username).first()
+    return user
+   
