3e2bd59c14235bac716fe1ab0547230df75dde72
milad fathi <miladfathimf7@gmail.com>
2024-03-14 15:20:42 -0700

    APIs created to sign up, login and activate users

--- Backend/app/IAM/auth.py
+++ Backend/app/IAM/auth.py
@@ -10,6 +10,19 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import JWTError, jwt
 
+import redis
+
+import random
+
+
+
+pool = redis.ConnectionPool(host='redis', port=6379, decode_responses=True)
+r = redis.Redis(connection_pool=pool)
+
+
+
+
+
 
 router = APIRouter(
     prefix='/auth',
@@ -43,7 +56,7 @@ class PersonRequest(BaseModel):
     plain_text_password: str = Field(min_length=1)
     # role: str = Field(min_length=1)
     email: str = Field(min_length=1)
-    phone_number:int = Field(min_length=10, max_length=16)
+    phone_number:int = Field(gt=0)
     # is_active: bool = Field(default=False) 
     
 class Token(BaseModel):
@@ -66,9 +79,9 @@ def authenticate_user(user_name: str, password: str, db):
     if not bcrypt_context.verify(password, user.hashed_password):
         # return {"msg": "Wrong password"}
         return False
-    # if user.is_active == False:
-    #     # return {"msg": "Activate first"}
-    #     return False
+    if user.is_active == False:
+        # return {"msg": "Activate first"}
+        return False
     return user
 
 
@@ -102,8 +115,21 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
 
 
 
+# *************************cast the code to str if needed
+# *************************id can be replaced with 'username'    &&    nx dont let code be replaced until its alive...
+# generate code and save it in redis
+def create_code(id: str):
+    code = str(random.randint(100000, 999999))
+    print(code)
+    r.set(f'{id}', f'{code}', ex=60, nx=True)
 
 
+# check the given code
+def verify_code(code: str, id: str):
+    if r.get(f'{id}') == code:
+        return True
+    else:
+        return False
 
 
 
@@ -125,6 +151,9 @@ async def create_user(db: db_dependency,
 
         db.add(create_user_model)
         db.commit()
+
+        create_code(str(create_user_model.id))
+        return create_user_model.id
     else:
         return{"msg":"The username is already existing!!!"}
 
@@ -147,12 +176,16 @@ async def create_admin(db: db_dependency,
 
         db.add(create_admin_model)
         db.commit()
+
+        create_code(str(create_admin_model.id))
+        return create_admin_model.id
     else:
         return{"msg":"The username is already existing!!!"}
 
+
 # *******************person req. class instead ....
 # login for user/admin
-@router.post("token",response_model=Token)
+@router.post("/token",response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
@@ -165,8 +198,41 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     return {'access_token': token, 'token_type': 'bearer'}
 
 
-@router.get("read_user")
+# to return person by it's username
+@router.get("/read_user")
 async def read_all_users(username:str,db:db_dependency):
     user = db.query(Person).filter(Person.user_name == username).first()
     return user
    
+
+
+# to activate  user/admin
+@router.put("activate_user/",status_code=status.HTTP_204_NO_CONTENT)
+async def activate_person(db: db_dependency,
+                          userid: str,
+                          code: str):
+    person_model = db.query(Person).filter(Person.id == userid).first()
+    if person_model is None:
+        raise HTTPException(status_code=404, detail="user not found,sign up first!!!")
+
+    # check the given code and make "activator" True ,if the code was correct 
+    activator = verify_code(code, userid)
+
+
+    if activator:
+        person_model.is_active = True
+        db.add(person_model)
+        db.commit()
+            
+
+@router.get("/generate_code",status_code=status.HTTP_200_OK)
+async def generate_code(id):
+    create_code(str(id))
+
+
+
+# @router.get("/test")
+# async def read_redis():
+#     # print(r.get('key2'))
+#     r.delete('key','key1','key2')
+#     return r.get('key2')
