1d94274620cbe4755d3fbb7f5fe913b5ea05db8c
M.A.T <mat.chenari.computer@gmail.com>
2024-03-15 23:56:16 +0330

    add first service

---
+++ Frontend/movie-land/src/app/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://your-api-url.com';
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials: {phonenumber: number, password: string}): Observable<any> {
+     return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+ 
+  signup(user: {phonenumber: number, password: string}): Observable<any> {
+     return this.http.post(`${this.apiUrl}/signup`, user);
+  }
+}
