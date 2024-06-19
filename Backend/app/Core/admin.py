0a9878111b2aa5712f9aefc89f30c511b9a870ad
milad fathi <miladfathimf7@gmail.com>
2024-06-19 16:37:50 -0700

    debug

--- Backend/app/Core/admin.py
+++ Backend/app/Core/admin.py
@@ -31,14 +31,14 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 class FilmRequest(BaseModel):
     title: str = Field(min_length=1)
     description: str = Field(min_length=5, max_length=250)
-    rating: int = Field(gt=0, lt=6 | None)
-    cover_link: str = Field(min_length=1 | None)
-    trailer_link: str = Field(min_length=1 | None)
-    date: str = Field(min_length=1 | None)
-    budget: int = Field(gt=0 | None)
-    language:str = Field(min_length=1 | None)
-    duration: int = Field(gt=0 | None)
-    article_link: str = Field(min_length=1 | None)
+    rating: int = Field(gt=0, lt=6 )
+    cover_link: str = Field(min_length=1)
+    trailer_link: str = Field(min_length=1 )
+    date: str = Field(min_length=1 )
+    budget: int = Field(gt=0)
+    language:str = Field(min_length=1)
+    duration: int = Field(gt=0)
+    article_link: str = Field(min_length=1)
 
 
 @router.post("/addFilm", status_code=status.HTTP_201_CREATED)
